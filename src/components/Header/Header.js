c303c22a3e801ffb2e00be26d38554dfc1e002aa
PrLamkin <shiektheimmortal@outlook.com>
2020-12-07 11:40:08 -0600

    Header set up and mapped out

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            //I'll be putting the API call at the top here along with account info
+            
+        </div>
+    )
+}
+
+export default Header
